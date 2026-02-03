7d8f1c1a73338b96b0e5fb5ef64e2640b0eeae96
Snaptraks <snaptraks@gmail.com>
2026-02-03 13:55:00 -0500

    Fix Ruff errors in hexoskin.data

--- src/epilepsy_tools/hexoskin/data.py
+++ src/epilepsy_tools/hexoskin/data.py
@@ -14,25 +14,18 @@ if TYPE_CHECKING:
     from os import PathLike
     from typing import Literal, TypedDict
 
-    SignalHeaderDict = TypedDict(
-        "SignalHeaderDict",
-        {
-            "label": str,
-            "sample_frequency": float,
-            "dimension": str,
-        },
-    )
-    HeaderDict = TypedDict(
-        "HeaderDict",
-        {
-            "patientname": str,
-            "sex": str,
-            "startdate": datetime,
-            "birthdate": str,
-            "recording_additional": str,
-            "patient_additional": str,
-        },
-    )
+    class SignalHeaderDict(TypedDict):
+        label: str
+        sample_frequency: float
+        dimension: str
+
+    class HeaderDict(TypedDict):
+        patientname: str
+        sex: str
+        startdate: datetime
+        birthdate: str
+        recording_additional: str
+        patient_additional: str
 
 
 _log = logging.getLogger(__name__)
@@ -50,6 +43,7 @@ class SignalHeader:
         The sample rate in Hz of the signal.
     dimension : :class:`str`
         The units of the signal.
+
     """
 
     label: str
@@ -60,6 +54,7 @@ class SignalHeader:
 @dataclass
 class RecordingInfo:
     """Stores metadata of the recording (raw signals).
+
     Construct with :class:`RecordingInfo.from_file`.
 
     Attributes
@@ -78,6 +73,7 @@ class RecordingInfo:
         The ID on Hexoskin's platform of the patient of the recording.
     signals : list[:class:`SignalHeader`]
         The list of signals (channels) in the recording.
+
     """
 
     patient_name: str
@@ -101,11 +97,11 @@ class RecordingInfo:
         -------
         :class:`RecordingInfo`
             The information of the recording.
-        """
 
+        """
         with pyedflib.EdfReader(str(file)) as reader:
-            signal_headers: list[SignalHeaderDict] = reader.getSignalHeaders()  # type: ignore
-            header: HeaderDict = reader.getHeader()  # type: ignore
+            signal_headers: list[SignalHeaderDict] = reader.getSignalHeaders()  # type: ignore[reportAssignmentType]
+            header: HeaderDict = reader.getHeader()  # type: ignore[reportAssignmentType]
 
         signals = [
             SignalHeader(
@@ -120,7 +116,7 @@ class RecordingInfo:
             patient_name=header["patientname"].replace(" ", ""),
             sex=header["sex"],
             start_time=header["startdate"],
-            birth_date=datetime.strptime(header["birthdate"], "%d %b %Y").date(),
+            birth_date=datetime.strptime(header["birthdate"], "%d %b %Y").date(),  # noqa: DTZ007
             hexoskin_record_id=int(
                 header["recording_additional"].removeprefix("hexoskin_record_id=")
             ),
@@ -133,6 +129,7 @@ class RecordingInfo:
 
 def _parse_label(label: str) -> str:
     """Extract the name of the channel from the label.
+
     Labels have a format of <ID:Name>, so re discard the ID: part.
 
     Parameters
@@ -144,6 +141,7 @@ def _parse_label(label: str) -> str:
     -------
     :class:`str`
         The Name part of the original label.
+
     """
     return label[label.index(":") + 1 :]
 
@@ -151,8 +149,9 @@ def _parse_label(label: str) -> str:
 def generate_timestamps(
     start_time: datetime, sample_rate: float, length: int
 ) -> pd.DatetimeIndex:
-    """Generate the timestamps for the given parameters. To be used as the
-    index of a DataFrame.
+    """Generate the timestamps for the given parameters.
+
+    To be used as the index of a DataFrame.
 
     Parameters
     ----------
@@ -167,6 +166,7 @@ def generate_timestamps(
     -------
     timestamps : :class:`pandas.DatetimeIndex`
         A :class:`pandas.Index` instance of :class:`datetime.datetime` objects.
+
     """
     timestamps = pd.date_range(
         start=start_time,
@@ -222,10 +222,12 @@ def load_data(
     ------
     :exc:`ValueError`
         The file provided is not a .edf file.
+
     """
     _log.debug(f"reading file {file}")
     if Path(file).suffix.lower() != ".edf":
-        raise ValueError(f"{file} is not a .edf file")
+        msg = f"{file} is not a .edf file"
+        raise ValueError(msg)
 
     # make sure file is a str for pyedflib
     signals, signal_headers, header = pyedflib.highlevel.read_edf(str(file))
@@ -245,7 +247,7 @@ def load_data(
     )
 
     data = pd.DataFrame(index=timestamps)
-    for signal, signal_header in zip(signals, signal_headers):
+    for signal, signal_header in zip(signals, signal_headers, strict=True):
         metric_data = np.full(max_length, fill_value=np.nan)
         metric_data[:: int(max_sample_rate / signal_header["sample_frequency"])] = (
             signal
@@ -255,6 +257,6 @@ def load_data(
     if not as_dataframe:
         _log.debug("Returning data in a dict of pandas.Series.")
         return {col: data[col].dropna() for col in data.columns}
-    else:
-        _log.debug("Returning data in a pandas.DataFrame.")
-        return data
+
+    _log.debug("Returning data in a pandas.DataFrame.")
+    return data
